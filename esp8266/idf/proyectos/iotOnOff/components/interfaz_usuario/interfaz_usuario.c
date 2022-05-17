2315e38378d53fc6e568921d46cc842f34368c22
r126401 <r126401@gmail.com>
2022-05-17 21:26:58 +0200

    Se modifica accion_Temporizada para meter appuser_cambiar_modo_aplicacion(datosApp, NORMAL_AUTO);

--- esp8266/idf/proyectos/iotOnOff/components/interfaz_usuario/interfaz_usuario.c
+++ esp8266/idf/proyectos/iotOnOff/components/interfaz_usuario/interfaz_usuario.c
@@ -230,6 +230,7 @@ void appuser_ejecucion_accion_temporizada(void *datosApp) {
     if (respuesta != NULL) {
     	publicar_mensaje_json(datosApp, respuesta, NULL);
     }
+    appuser_cambiar_modo_aplicacion(datosApp, NORMAL_AUTO);
     ESP_LOGI(TAG, ""TRAZAR"FIN DE LA TEMPORIZACION. RELE APAGADO", INFOTRAZA);
 }
 
